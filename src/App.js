1154df4f497a8286542a6bacca8702f1bd5d7847
Natali Dvinskikh <dvinskikhnatali@gmail.com>
2023-01-30 21:45:36 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
+import { data } from './data';
 import './App.css';
 
 function App() {
+
+const [point, setPoint] = useState(0);
+const {id, place, image} = data[point];
+
+const previousPoint = () => {
+  setPoint((point => {
+    point --;
+    if(point<0) {
+      point =  data.length-1;
+    }
+    return point
+  }))
+}
+
+const nextPoint = () => {
+      setPoint((point =>{
+        point++;
+        if(point>data.length - 1) {
+          point = 0;
+        }
+          return point;
+      }))
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='container'>
+        <h1>Amazing places in Israel</h1>
+      </div>
+      <div className='container'>
+        <img src={image} width="300px" alt ="place"/>
+      </div>
+      <div className='container'>
+        <h2>{id} - {place}</h2>
+      </div>
+      <div className='btn container'>
+        <button onClick={previousPoint}>Previous</button>
+        <button onClick={nextPoint}>Next</button>
+      </div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,76 @@
+export const data = [
+
+    {
+
+      id: 1,
+
+      place: "Shokeda",
+
+      image: "https://images.unsplash.com/photo-1582827026232-a595129deebe?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
+
+
+    },
+
+    {
+
+      id: 2,
+
+      place: "Masada",
+
+      image: "https://images.unsplash.com/photo-1592989847276-d9d6a8dab73d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
+
+    },
+
+    {
+
+      id: 3,
+
+      place: "Red Canyon",
+
+      image: "https://images.unsplash.com/photo-1624727513770-67b91ad1d513?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
+
+    },
+
+
+
+    {
+
+      id: 4,
+
+      place: "Palmachim Beach National Park",
+
+      image: "https://images.unsplash.com/photo-1541344302578-0ea37e27491d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
+
+    },
+
+    {
+
+      id: 5,
+
+      place: "Rosh Hanikra",
+
+      image: "https://images.unsplash.com/photo-1506183915311-3c4749d0ac8e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1674&q=80"
+
+    },
+
+    {
+
+        id: 6,
+
+        place: "Nana Estate Winery",
+
+        image: "https://images.unsplash.com/photo-1662960501187-1e043a43ef35?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1036&q=80"
+
+      },
+
+      {
+
+        id: 7,
+
+        place: "Selina Ramon, Mitzpe Ramon",
+
+        image: "https://images.unsplash.com/photo-1662960158221-f76b79584491?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1036&q=80"
+
+      },
+
+  ]
